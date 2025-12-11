1ec0c692ef8ba605c77ded602a0bd1407caceaab
Youssef Bouraoui <bouraouiyoussef12@gmail.com>
2025-12-12 00:49:20 +0100

    added client laoyout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { OrganizerPageLayout } from './shared/OrganizerPageLayout';
+import { OrganizerPageLayout } from './shared/ClientPageLayout';
 import { EventsListPage } from './pages/EventPages/EventListPage/EventsListPage';
 import LoginPage from './pages/auth/LoginPage';
 import SignUpPage from './pages/auth/SignUpPage';
 import CreateEventPage from './pages/EventPages/CreateEvenetPage';
+import ProfilePage from './pages/clientPages/ProfilePage';
+import { ClientPageLayout } from './shared/ClientLayoutPage';
 
 function App() {
 
@@ -17,6 +19,11 @@ function App() {
           <Route path='organizer' element={<OrganizerPageLayout />}>
             <Route path="events" element={<EventsListPage />} />
             <Route path="create-event" element={<CreateEventPage />} />
+          </Route>
+
+          <Route path='client' element={<ClientPageLayout/>}>
+            <Route path="events"  />
+            <Route path="profile" element={<ProfilePage />} />
           </Route>
         </Routes>
       </Router>
